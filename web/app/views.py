48182ae62fc75b21d44f9fab03c145d026766417
ken <thanatonken@gmail.com>
2023-02-06 21:14:24 +0700

    lab10

--- web/app/views.py
+++ web/app/views.py
@@ -1,10 +1,9 @@
-
 from flask import (jsonify, render_template,
                    request, url_for, flash, redirect)
 import json
+from sqlalchemy.sql import text
 from app import app
-
-
+from app import db
 
 
 @app.route('/')
@@ -14,7 +13,18 @@ def home():
 
 
 
+@app.route('/db')
+def db_connection():
+    try:
+        with db.engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        return '<h1>db works.</h1>'
+    except Exception as e:
+        return '<h1>db is broken.</h1>' + str(e)
+
+
+
+
 @app.route('/lab04')
 def lab04_bootstrap():
     return app.send_static_file('lab04_bootstrap.html')
-
